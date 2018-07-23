831a7dc679f65f72f25ae7419485db8a40d44fe3
Patricia Nykaza <pnykaza@calstatela.edu>
2018-07-23 19:04:47 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,36 @@
+require 'pry'
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = 'pending'
+  end
+  
+  def valid?()
+    return @status == 'pending' && @sender.valid?() && @receiver.valid?()
+  end
+  
+  def execute_transaction()
+    if @sender.balance < 0 || @receiver.balance < 0
+    # if !@sender.valid?() && !@receiver.valid?() 
+      @status = 'rejected'
+      return "Transaction rejected. Please check your account balance."
+    # elsif valid?() 
+    #   sender.balance -= amount 
+    #   receiver.balance += amount
+    #   @status = 'complete'
+    end
+  end
+  
+  def reverse_transfer()
+    if status == 'complete'
+      sender.balance += amount 
+      receiver.balance -= amount
+      @status = 'reversed'
+    end 
+  end
+  
 end
